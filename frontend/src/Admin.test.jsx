cf95c40477aabf08a2215f679f4f13815f797858
gramdel <legovec96@gmail.com>
2025-04-01 18:16:47 +0300

    Тесты для Admin.jsx написаны

--- frontend/src/Admin.test.jsx
+++ frontend/src/Admin.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
@@ -22,27 +22,44 @@ const renderWithContext = () => render(
     </LocalizationProvider>
 );
 
+const product = {
+    id: "1",
+    name: "iPhone 16",
+    price: "100000",
+    avgRating: "4.9",
+    reviewCount: "10000",
+    releaseDate: "2024-09-08T21:00:00.000Z",
+    countryId: "2",
+    manufacturerId: "3",
+}
+const { id, ...productWithoutId } = product;
+
 describe("Admin component", () => {
-    let addButton, updateButton, deleteButton;
-    let id, name, price, avgRating, reviewCount, releaseDate, countryId, manufacturerId;
+    let addButton, updateButton, deleteButton, resetButton, executeButton;
+    let inputsMap = {};
 
     beforeEach(() => {
+        vi.restoreAllMocks();
         renderWithContext();
         addButton = screen.getByText("Добавление");
         updateButton = screen.getByText("Изменение");
         deleteButton = screen.getByText("Удаление");
+        resetButton = screen.getByText("Сброс полей");
+        executeButton = screen.getByText("Выполнить");
     });
 
     describe("Режим добавления", () => {
         beforeEach(() => {
-            id = screen.queryByLabelText("ID");
-            name = screen.queryByLabelText("Имя");
-            price = screen.queryByLabelText("Цена, ₽");
-            avgRating = screen.queryByLabelText("Рейтинг");
-            reviewCount = screen.queryByLabelText("Количество отзывов");
-            releaseDate = screen.queryByLabelText("Дата выпуска");
-            countryId = screen.queryByLabelText("Страна производства");
-            manufacturerId = screen.queryByLabelText("Производитель");
+            inputsMap = {
+                id: screen.queryByLabelText("ID"),
+                name: screen.queryByLabelText("Имя"),
+                price: screen.queryByLabelText("Цена, ₽"),
+                avgRating: screen.queryByLabelText("Рейтинг"),
+                reviewCount: screen.queryByLabelText("Количество отзывов"),
+                releaseDate: screen.queryByLabelText("Дата выпуска"),
+                countryId: screen.queryByLabelText("Страна производства"),
+                manufacturerId: screen.queryByLabelText("Производитель")
+            };
         });
 
         it("Должен быть выбран по умолчанию", () => {
@@ -52,28 +69,97 @@ describe("Admin component", () => {
         });
 
         it("Должен позволять изменить все параметры, кроме id", () => {
-            expect(id).not.toBeInTheDocument();
-            expect(name).toBeInTheDocument();
-            expect(price).toBeInTheDocument();
-            expect(avgRating).toBeInTheDocument();
-            expect(reviewCount).toBeInTheDocument();
-            expect(releaseDate).toBeInTheDocument();
-            expect(countryId).toBeInTheDocument();
-            expect(manufacturerId).toBeInTheDocument();
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "id") {
+                    expect(value).not.toBeInTheDocument();
+                } else {
+                    expect(value).toBeInTheDocument();
+                }
+            });
+        });
+
+        it("Должен добавлять продукт", async () => {
+            global.fetch = vi.fn(() => Promise.resolve({
+                ok: true,
+                status: 201,
+                text: () => Promise.resolve("1")
+            }));
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "releaseDate") {
+                    fireEvent.change(value, { target: { value: "09.09.24" } });
+                } else if (key !== "id") {
+                    fireEvent.change(value, { target: { value: product[key] } });
+                }
+            });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products");
+            expect(options.method).toBe("POST");
+            expect(options.body).toBe(JSON.stringify(productWithoutId));
+            expect(screen.queryByText("Продукт успешно добавлен! Его ID: 1")).toBeInTheDocument();
+        });
+
+        it("Не должен добавлять продукт при ошибке сервера", async () => {
+            global.fetch = vi.fn(() => Promise.resolve({
+                ok: false,
+                status: 500,
+                text: () => Promise.resolve("Internal error")
+            }));
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "releaseDate") {
+                    fireEvent.change(value, { target: { value: "09.09.24" } });
+                } else if (key !== "id") {
+                    fireEvent.change(value, { target: { value: product[key] } });
+                }
+            });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products");
+            expect(options.method).toBe("POST");
+            expect(options.body).toBe(JSON.stringify(productWithoutId));
+            expect(screen.queryByText("При добавлении произошла ошибка! Код: 500")).toBeInTheDocument();
+        });
+
+        it("Не должен добавлять продукт при ошибке связи", async () => {
+            global.fetch = vi.fn(() => Promise.reject());
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "releaseDate") {
+                    fireEvent.change(value, { target: { value: "09.09.24" } });
+                } else if (key !== "id") {
+                    fireEvent.change(value, { target: { value: product[key] } });
+                }
+            });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products");
+            expect(options.method).toBe("POST");
+            expect(options.body).toBe(JSON.stringify(productWithoutId));
+            expect(screen.queryByText("При добавлении произошла ошибка связи!")).toBeInTheDocument();
         });
     });
 
     describe("Режим изменения", () => {
         beforeEach(() => {
             fireEvent.click(updateButton);
-            id = screen.queryByLabelText("ID");
-            name = screen.queryByLabelText("Имя");
-            price = screen.queryByLabelText("Цена, ₽");
-            avgRating = screen.queryByLabelText("Рейтинг");
-            reviewCount = screen.queryByLabelText("Количество отзывов");
-            releaseDate = screen.queryByLabelText("Дата выпуска");
-            countryId = screen.queryByLabelText("Страна производства");
-            manufacturerId = screen.queryByLabelText("Производитель");
+            inputsMap = {
+                id: screen.queryByLabelText("ID"),
+                name: screen.queryByLabelText("Имя"),
+                price: screen.queryByLabelText("Цена, ₽"),
+                avgRating: screen.queryByLabelText("Рейтинг"),
+                reviewCount: screen.queryByLabelText("Количество отзывов"),
+                releaseDate: screen.queryByLabelText("Дата выпуска"),
+                countryId: screen.queryByLabelText("Страна производства"),
+                manufacturerId: screen.queryByLabelText("Производитель")
+            };
         });
 
         it("Должен быть включен после нажатии кнопки \"Изменение\"", () => {
@@ -83,28 +169,85 @@ describe("Admin component", () => {
         });
 
         it("Должен позволять изменить все параметры", () => {
-            expect(id).toBeInTheDocument();
-            expect(name).toBeInTheDocument();
-            expect(price).toBeInTheDocument();
-            expect(avgRating).toBeInTheDocument();
-            expect(reviewCount).toBeInTheDocument();
-            expect(releaseDate).toBeInTheDocument();
-            expect(countryId).toBeInTheDocument();
-            expect(manufacturerId).toBeInTheDocument();
+            Object.values(inputsMap).forEach((value) => expect(value).toBeInTheDocument());
+        });
+
+        it("Должен изменять продукт с id 1", async () => {
+            global.fetch = vi.fn(() => Promise.resolve({ ok: true, status: 200 }));
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "releaseDate") {
+                    fireEvent.change(value, { target: { value: "09.09.24" } });
+                } else {
+                    fireEvent.change(value, { target: { value: product[key] } });
+                }
+            });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products/1");
+            expect(options.method).toBe("PUT");
+            expect(options.body).toBe(JSON.stringify(product));
+            expect(screen.queryByText("Продукт с ID 1 успешно обновлен!")).toBeInTheDocument();
+        });
+
+        it("Не должен изменять продукт с несуществующим id 2", async () => {
+            global.fetch = vi.fn(() => Promise.resolve({ ok: false, status: 404 }));
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "releaseDate") {
+                    fireEvent.change(value, { target: { value: "09.09.24" } });
+                } else if (key === "id") {
+                    fireEvent.change(value, { target: { value: "2" } });
+                } else {
+                    fireEvent.change(value, { target: { value: product[key] } });
+                }
+            });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products/2");
+            expect(options.method).toBe("PUT");
+            expect(options.body).toBe(JSON.stringify({ ...product, id: "2" }));
+            expect(screen.queryByText("При обновлении произошла ошибка! Код: 404")).toBeInTheDocument();
+        });
+
+        it("Не должен изменять продукт при ошибке связи", async () => {
+            global.fetch = vi.fn(() => Promise.reject());
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "releaseDate") {
+                    fireEvent.change(value, { target: { value: "09.09.24" } });
+                } else {
+                    fireEvent.change(value, { target: { value: product[key] } });
+                }
+            });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products/1");
+            expect(options.method).toBe("PUT");
+            expect(options.body).toBe(JSON.stringify(product));
+            expect(screen.queryByText("При обновлении произошла ошибка связи!")).toBeInTheDocument();
         });
     });
 
     describe("Режим удаления", () => {
         beforeEach(() => {
             fireEvent.click(deleteButton);
-            id = screen.queryByLabelText("ID");
-            name = screen.queryByLabelText("Имя");
-            price = screen.queryByLabelText("Цена, ₽");
-            avgRating = screen.queryByLabelText("Рейтинг");
-            reviewCount = screen.queryByLabelText("Количество отзывов");
-            releaseDate = screen.queryByLabelText("Дата выпуска");
-            countryId = screen.queryByLabelText("Страна производства");
-            manufacturerId = screen.queryByLabelText("Производитель");
+            inputsMap = {
+                id: screen.queryByLabelText("ID"),
+                name: screen.queryByLabelText("Имя"),
+                price: screen.queryByLabelText("Цена, ₽"),
+                avgRating: screen.queryByLabelText("Рейтинг"),
+                reviewCount: screen.queryByLabelText("Количество отзывов"),
+                releaseDate: screen.queryByLabelText("Дата выпуска"),
+                countryId: screen.queryByLabelText("Страна производства"),
+                manufacturerId: screen.queryByLabelText("Производитель")
+            };
         });
 
         it("Должен быть включен после нажатии кнопки \"Удаление\"", () => {
@@ -114,29 +257,52 @@ describe("Admin component", () => {
         });
 
         it("Должен позволять изменить только id", () => {
-            expect(id).toBeInTheDocument();
-            expect(name).not.toBeInTheDocument();
-            expect(price).not.toBeInTheDocument();
-            expect(avgRating).not.toBeInTheDocument();
-            expect(reviewCount).not.toBeInTheDocument();
-            expect(releaseDate).not.toBeInTheDocument();
-            expect(countryId).not.toBeInTheDocument();
-            expect(manufacturerId).not.toBeInTheDocument();
+            Object.entries(inputsMap).forEach(([key, value]) => {
+                if (key === "id") {
+                    expect(value).toBeInTheDocument();
+                } else {
+                    expect(value).not.toBeInTheDocument();
+                }
+            });
+        });
+
+        it("Должен удалять продукт с id 1", async () => {
+            global.fetch = vi.fn(() => Promise.resolve({ ok: true, status: 200 }));
+            fireEvent.change(inputsMap.id, { target: { value: "1" } });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products/1");
+            expect(options.method).toBe("DELETE");
+            expect(screen.queryByText("Продукт с ID 1 успешно удален!")).toBeInTheDocument();
+        });
+
+        it("Не должен удалять продукт с несуществующим id 2", async () => {
+            global.fetch = vi.fn(() => Promise.resolve({ ok: false, status: 404 }));
+            fireEvent.change(inputsMap.id, { target: { value: "2" } });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products/2");
+            expect(options.method).toBe("DELETE");
+            expect(screen.queryByText("При удалении произошла ошибка! Код: 404")).toBeInTheDocument();
+        });
+
+        it("Не должен удалять продукт при ошибке связи", async () => {
+            global.fetch = vi.fn(() => Promise.reject());
+            fireEvent.change(inputsMap.id, { target: { value: "1" } });
+            fireEvent.click(executeButton);
+
+            await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+            const [url, options] = global.fetch.mock.calls[0];
+
+            expect(url).toBe("http://localhost:8080/api/products/1");
+            expect(options.method).toBe("DELETE");
+            expect(screen.queryByText("При удалении произошла ошибка связи!")).toBeInTheDocument();
         });
     });
-
-    /*
-    it("должен отправлять запрос при клике", async () => {
-        render(
-            <SnackbarProvider>
-                <Admin/>
-            </SnackbarProvider>
-        );
-
-        fireEvent.click(screen.getByText("Отправить"));
-
-        await waitFor(() => {
-            expect(global.fetch).toHaveBeenCalledWith("/api/some-endpoint", expect.any(Object));
-        });
-    });*/
 });

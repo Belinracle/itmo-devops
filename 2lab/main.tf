ba781872045ad04d5b41f887e5e56914f91497be
Daniel Dzhukashev <rosdanas@gmail.com>
2025-04-07 23:02:39 +0300

    terrraform 2

--- 2lab/main.tf
+++ 2lab/main.tf
@@ -1,52 +1,40 @@
-resource "libvirt_pool" "pool" {
-  name = "${var.prefix}pool"
-  type = "dir"
-  path = "${var.pool_path}${var.prefix}pool"
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.7.0"
+    }
+  }
 }
 
-resource "libvirt_volume" "image" {
-  name   = var.image.name
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+resource "libvirt_volume" "ubuntu-qcow2" {
+  name   = "ubuntu.qcow2"
+  pool   = "default"
   format = "qcow2"
-  pool   = libvirt_pool.pool.name
-  source = var.image.url
-}
-
-resource "libvirt_volume" "root" {
-  name           = "${var.prefix}root"
-  pool           = libvirt_pool.pool.name
-  base_volume_id = libvirt_volume.image.id
-  size           = var.vm.disk
+  source = "https://releases.ubuntu.com/22.04/ubuntu-22.04.3-live-server-amd64.iso"
 }
 
 resource "libvirt_domain" "vm" {
-  name   = "${var.prefix}master"
-  memory = var.vm.ram
-  vcpu   = var.vm.cpu
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
-  qemu_agent = true
-  autostart  = true
+  name = "my-vm"
+
+  memory = 2048
+  vcpu   = 2
 
   network_interface {
-    bridge         = var.vm.bridge
-    wait_for_lease = true
+    network_name = "default"
   }
 
   disk {
-    volume_id = libvirt_volume.root.id
+    volume_id = libvirt_volume.ubuntu-qcow2.id
   }
-}
 
-data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
-}
-
-data "template_file" "network_config" {
-  template = file("${path.module}/network_config.cfg")
-}
-
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "commoninit.iso"
-  pool           = libvirt_pool.pool.name
-  user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
 }

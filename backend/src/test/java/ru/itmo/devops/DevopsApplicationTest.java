ce438554e409559fb75c79f46d2b57823ed7fad1
Daniel Dzhukashev <rosdanas@gmail.com>
2025-05-29 00:44:26 +0300

    check fail coverage

--- backend/src/test/java/ru/itmo/devops/DevopsApplicationTest.java
+++ backend/src/test/java/ru/itmo/devops/DevopsApplicationTest.java
@@ -1,57 +1,57 @@
-package ru.itmo.devops;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-
-import java.util.ArrayList;
-import java.util.List;
-
-class DevopsApplicationTest {
-    private static final List<String> BASE_ARGS = List.of(
-            "--server.port=0",
-            "--spring.jpa.hibernate.ddl-auto=none",
-            "--spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults=false",
-            "--spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect"
-    );
-
-    @Test
-    void shouldFailWhenBotTokenNotSet() {
-        String[] args = new ArrayList<>(BASE_ARGS) {{
-            add("--tg.botToken=");
-            add("--tg.chatId=test");
-        }}.toArray(new String[0]);
-
-        Assertions.assertThrows(RuntimeException.class, () -> SpringApplication.run(DevopsApplication.class, args));
-    }
-
-    @Test
-    void shouldFailWhenChatIdNotSet() {
-        String[] args = new ArrayList<>(BASE_ARGS) {{
-            add("--tg.botToken=test");
-            add("--tg.chatId=");
-        }}.toArray(new String[0]);
-
-        Assertions.assertThrows(RuntimeException.class, () -> SpringApplication.run(DevopsApplication.class, args));
-    }
-
-    @Test
-    void shouldFailWhenEnvNotSet() {
-        String[] args = new ArrayList<>(BASE_ARGS) {{
-            add("--tg.botToken=");
-            add("--tg.chatId=");
-        }}.toArray(new String[0]);
-
-        Assertions.assertThrows(RuntimeException.class, () -> SpringApplication.run(DevopsApplication.class, args));
-    }
-
-    @Test
-    void shouldRunWhenEnvSet() {
-        String[] args = new ArrayList<>(BASE_ARGS) {{
-            add("--tg.botToken=test");
-            add("--tg.chatId=test");
-        }}.toArray(new String[0]);
-
-        Assertions.assertDoesNotThrow(() -> SpringApplication.run(DevopsApplication.class, args));
-    }
-}
+//package ru.itmo.devops;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.SpringApplication;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//class DevopsApplicationTest {
+//    private static final List<String> BASE_ARGS = List.of(
+//            "--server.port=0",
+//            "--spring.jpa.hibernate.ddl-auto=none",
+//            "--spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults=false",
+//            "--spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect"
+//    );
+//
+//    @Test
+//    void shouldFailWhenBotTokenNotSet() {
+//        String[] args = new ArrayList<>(BASE_ARGS) {{
+//            add("--tg.botToken=");
+//            add("--tg.chatId=test");
+//        }}.toArray(new String[0]);
+//
+//        Assertions.assertThrows(RuntimeException.class, () -> SpringApplication.run(DevopsApplication.class, args));
+//    }
+//
+//    @Test
+//    void shouldFailWhenChatIdNotSet() {
+//        String[] args = new ArrayList<>(BASE_ARGS) {{
+//            add("--tg.botToken=test");
+//            add("--tg.chatId=");
+//        }}.toArray(new String[0]);
+//
+//        Assertions.assertThrows(RuntimeException.class, () -> SpringApplication.run(DevopsApplication.class, args));
+//    }
+//
+//    @Test
+//    void shouldFailWhenEnvNotSet() {
+//        String[] args = new ArrayList<>(BASE_ARGS) {{
+//            add("--tg.botToken=");
+//            add("--tg.chatId=");
+//        }}.toArray(new String[0]);
+//
+//        Assertions.assertThrows(RuntimeException.class, () -> SpringApplication.run(DevopsApplication.class, args));
+//    }
+//
+//    @Test
+//    void shouldRunWhenEnvSet() {
+//        String[] args = new ArrayList<>(BASE_ARGS) {{
+//            add("--tg.botToken=test");
+//            add("--tg.chatId=test");
+//        }}.toArray(new String[0]);
+//
+//        Assertions.assertDoesNotThrow(() -> SpringApplication.run(DevopsApplication.class, args));
+//    }
+//}
